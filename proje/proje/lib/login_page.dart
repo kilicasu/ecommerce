5cff52c33d3e6cb6fb1db0831a838343fe244c85
Asuman Kılıç <asumankilic2003@gmail.com>
2024-07-10 13:29:45 +0300

    first-commit

---
+++ proje/proje/lib/login_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("lib/assets/images/header.png"))),
+            ),
+            Text("BAVARIA"),
+          ],
+        ),
+      ),
+    );
+  }
+}
